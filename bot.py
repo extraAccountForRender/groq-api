c43039cb750207cdd881a06dda6ffd464aef604e
extraAccountForRender <adityakukreti009@gmail.com>
2024-07-24 16:21:39 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -9,6 +9,7 @@ class ConversationBot:
         Rules:
         1. whatever reply you give me, make sure to enclose headings with <h><h>, subheadings within <sb><sb> and content within <i><i>. 
         2. Do not give any other detail or message from your side.
+        3. If no previous message is provided, then simply answer the query.
         """
 
 
